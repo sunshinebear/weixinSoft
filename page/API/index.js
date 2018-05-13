be5b3efa8c8eb1e61760bf27eb89152fe9f9b77a
shaolong.yin <shaolong.yin@jimu.com>
2018-05-13 23:01:19 +0800

    add wxs

--- page/API/index.js
+++ page/API/index.js
@@ -1,3 +1,20 @@
 Page({
+	start_btn:function (argument) {
+		// body...
+		//console.log("abc");
+	},
+	fail_btn:function (argument) {
+		// body...
+	},
+	back_btn:function(){
 
+
+	},
+	forward_btn:function (argument) {
+		// body...
+	},
+	alert_btn:function (argument) {
+		// body...
+
+	}
 })
